a16c939a128b76d5422fdcd4275a51cadfca1eb5
Álvaro <alvarora12@gmail.com>
2021-12-19 16:32:37 +0100

    Add description to day19 tests

--- tests/day19.test.js
+++ tests/day19.test.js
@@ -1,25 +1,25 @@
 import learn from '../src/day19'
 
 describe('Day 19 challenge', () => {
-    test('Items without parenthesis but with repetitions', () => {
+    test('The first and the third courses are the solution', () => {
         expect(learn(10, [2, 3, 8, 1, 4])).toEqual([0, 2]);
     });
-    test('Items without parenthesis but with repetitions', () => {
+    test('The middle courses are the solution', () => {
         expect(learn(15, [2, 10, 4, 1])).toEqual([1, 2]);
     });
-    test('Items without parenthesis but with repetitions', () => {
+    test('The first two courses are the solution', () => {
         expect(learn(25, [10, 15, 20, 5])).toEqual([0, 1]);
     });
-    test('Items without parenthesis but with repetitions', () => {
+    test('The first course duration is the same as the requested one, so wo need other combination', () => {
         expect(learn(8, [8, 2, 1])).toEqual([1, 2]);
     });
-    test('Items without parenthesis but with repetitions', () => {
+    test('Last two courses', () => {
         expect(learn(8, [8, 2, 1, 4, 3])).toEqual([3, 4]);
     });
-    test('Items without parenthesis but with repetitions', () => {
+    test('All courses has a longer duration than the requested one', () => {
         expect(learn(4, [10, 14, 20])).toBeNull();
     });
-    test('Items without parenthesis but with repetitions', () => {
+    test('All courses has the same duration, impossible to match with other courses', () => {
         expect(learn(5, [5, 5, 5])).toBeNull();
     });
 })
